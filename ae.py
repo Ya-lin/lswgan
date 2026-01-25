a23541b74ff8fc9e7a56308fe5b0d909756dcfa8
Yalin Liao <46334839+Ya-lin@users.noreply.github.com>
2026-01-25 11:02:56 -0500

    update

--- ae.py
+++ ae.py
@@ -116,13 +116,14 @@ if  __name__ == "__main__":
             if(i%50==0):
                 test_samples = next(iter(test_loader))
                 loss , test_output = test_batch(test_samples)
-                utils.plotPCbatch(test_samples, test_output, show=False, save=True, name = (args.output_folder  + "epoch_" + str(i)))
+                utils.plotPCbatch(test_samples, test_output, show=False, save=True, 
+                                  name = (args.output_folder / "epoch_" + str(i)))
 
         else : # display all outputs
             
             test_samples = next(iter(test_loader))
             loss , test_output = test_batch(test_samples)
-            utils.plotPCbatch(test_samples,test_output)
+            utils.plotPCbatch(test_samples, test_output)
 
             print(writeString)
 
