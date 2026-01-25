5f82d07e1b7de54d9176318d5c7e0135d9ae575b
Yalin Liao <46334839+Ya-lin@users.noreply.github.com>
2026-01-25 16:58:19 -0500

    update

--- test_model.py
+++ test_model.py
@@ -1,45 +1,31 @@
 #%%
-import warnings
-warnings.filterwarnings('ignore')
-
-
-#%%
-import yaml
 import torch
 import numpy as np
-from box import Box
-from pathlib import Path
-from dataloaders import GetDataLoaders
+from config import Config
 from wgan_utils import generate_sample
 from utils import set_seed, plotPCbatch
 from models import PointCloudAE, Generator
 
-
-#%%
-# with open("config.yaml", "r") as file:
-#     args = yaml.safe_load(file)
-# args = Box(args)
-# print(args)
-
-# args.data_dir = Path.home()/args.data_dir
-# args.model_dir = Path.home()/args.model_dir
-
-# data_file = args.data_dir/args.dataset
-# pc_array = np.load(data_file)
-# print(f"data shape: {pc_array.shape}.")
-
-# point_size = pc_array.shape[1]
-# print(f"number of points in one 3D cloud: {point_size}.")
-# loader, _ = GetDataLoaders(npArray=pc_array, 
-#                            batch_size=args.batch_size, 
-#                            train_set_percentage=1.0)
+import warnings
+warnings.filterwarnings('ignore')
 
 
 #%% 
 if __name__ == "__main__":
 
+    # set random seed to make results reproducible
+    set_seed()
 
+    # load config and data
+    args = Config()
 
+    data_file = args.data_dir/args.dataset
+    pc_array = np.load(data_file)
+    print(f"data shape: {pc_array.shape}.")
+
+    # Assuming all models have the same size,
+    point_size = pc_array.shape[1]
+    print(f"number of points in one 3D cloud: {point_size}.")
     
     # load saved ae
     ae = PointCloudAE(point_size, args.latent_size).to(args.device)
@@ -55,7 +41,7 @@ if __name__ == "__main__":
     generator.eval()
     print("Successfully load wgan generator!")
 
-    #display generated samples
+    #display generated samples by saved models
     sample1 = generate_sample(generator, ae, args.batch_size, args.device, latent_dim=128)
     sample2 = generate_sample(generator, ae, args.batch_size, args.device, latent_dim=128)
     plotPCbatch(sample1, sample2)
